fbeca1d566593737be80596744cbe90b4c4026b0
Marcantonio Moraes <marcantonio.lupercino@gmail.com>
2025-08-11 23:24:30 -0300

    nome

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log ("Aqui")
+const rl = require ("node:readline")
+
+const prompt = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+let name;
+
+prompt.question("Qual o seu nome? ", answer => {
+    name = answer;
+    prompt.close()
+})
+
+prompt.on("close", () => {
+    console.log ("Seja bem vindo", name)
+})
+
