bde2e33939a894793f799b41b3c0a87b9c5aa5b1
Marcantonio Moraes <marcantonio.lupercino@gmail.com>
2025-08-12 18:36:51 -0300

    text

---
+++ src/text.js
@@ -0,0 +1,6 @@
+const name = "Marcos"
+const idade = 34
+const cidade = "Recife"
+ 
+console.log ("Meu nome é: ",name, "minha idade é: ",idade, )
+console.log (`Meu nome é ${name} e minha idade é ${idade}`)
