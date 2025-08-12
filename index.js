a82058405f5a89d10e0d1345dfb61e1bff6f58b2
Marcantonio Moraes <marcantonio.lupercino@gmail.com>
2025-08-11 22:37:40 -0300

    estudo

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require ('express')
 const app = express ()
 const PORT = 3000
 
+app.get ('/', () =>{
+    console.log('Entrou Aqui')
+    restart.send ('Hello Word')
+})
+
+
 app.listen(PORT, () => {
 console.log ('app online na porta ${PORT}')
 
