2ebba28dfdf641f867c83374edfd8845bb4f027b
Marcantonio Moraes <marcantonio.lupercino@gmail.com>
2025-08-11 22:08:21 -0300

    express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-let name = "Marcos"
-name = "Raquel"
-console.log (name)
+const name = "Marcos"
+const idade = 35
+const aniv = "24/01/1990"
+console.log ("Eu me chamo",name,"e tenho",idade,"e nasci no dia:",aniv,)
