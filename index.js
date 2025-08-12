715aca37207e6dd0ed833c25a2370bdc3cf303e3
Marcantonio Moraes <marcantonio.lupercino@gmail.com>
2025-08-11 22:51:56 -0300

    no

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require ('express')
+const express = require ("express")
 const app = express ()
-const PORT = 3000
+const port = 3000
 
-app.get ('/', () =>{
-    console.log('Entrou Aqui')
-    restart.send ('Hello Word')
+app.get ("/", (req, res) =>{
+    console.log("Entrou Aqui")
+    res.send ("Hello Word")
+})
+
+app.post("/", (req, res) =>{
+    res.send("App Post")
 })
 
 
-app.listen(PORT, () => {
-console.log ('app online na porta ${PORT}')
+app.listen(port, () => {
+console.log ('app online na porta',port,)
 
 })
 
