fb5bf6d15d603307f601ab807618114f3e074c10
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-16 01:41:42 -0300

    if,else,idade

--- src/text.js
+++ src/text.js
@@ -17,6 +17,43 @@ console.log (
     "Exelente"
 );*/
 
+// Strutura condicional 
+// if (expressão){}
+/*const sideA = 4;
+const sideB = 4;
+const sideC = 4;
+if (sideA === sideB && sideB === sideC){
+    console.log("O triangulo é equilátero")
+} else if (sideA === sideB || sideA === sideC || sideB === sideC){
+    console.log("O triangulo é isóceles")
+}else {
+    console.log("O triangulo é escaleno")
+};*/
+
+// Input de usuario
+const rl = require ("node:readline");
+const prompt = rl.createInterface({
+    input: process.stdin, output: process.stdout
+})
+
+prompt.question("Digite sua idade:",answer => {
+    const age = Number.parseInt(answer);
+    if (Number.isNaN(age)){
+        console.log("O que você digitou não é um numero valido!")
+    }else {
+        console.log (`Sua idade é ${age}`);
+        console.log(`Daqui a 4 anos você vai te ${age + 4} anos`);
+        console.log(`Daqui a 10 anos você vai te ${age + 10} anos`);
+        console.log(`Daqui a 15 anos você vai te ${age + 15} anos`);
+        console.log(`Daqui a 20 anos você vai te ${age + 20} anos`);
+    };
+    prompt.close();
+});
+
+
+
+
+
 
 
 
