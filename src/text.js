b541284324346da1470a2027148f28fa1c70a5b5
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-16 00:41:39 -0300

    Update text.js

--- src/text.js
+++ src/text.js
@@ -1,16 +1,13 @@
 // ternario 
-
-const hora = 18
-const vd = 100
-
+/*const hora = 18;
+const vd = 100;
 console.log(
     "Esta de",
     hora >= 0 && hora < 6 ? "Madrugada" :
     hora >= 6 && hora < 12 ? "dia" :
     hora >= 12 && hora < 18 ? "Tarde" :
     "Noite" 
-)
-
+);
 console.log (
     "Sua Saude é",
     vd >= 0 && vd < 10 ? "Pessima" :
@@ -18,5 +15,9 @@ console.log (
     vd >= 30 && vd < 60 ? "Boa" :
     vd >= 60 && vd < 90 ? "Ótima" :
     "Exelente"
-)
+);*/
+
+
+
+
 
