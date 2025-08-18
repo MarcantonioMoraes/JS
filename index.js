9d5b9c312b605bdbe353824bcaf8ac6ab8f999b2
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-18 02:43:03 -0300

    nota

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+const rl = require ("node:readline")
+const prompt = rl.createInterface({
+    input: process.stdin, output: process.stdout
+});
 
-const express = require (`express`
-const app = express ()
-const port = 3000
+prompt.question ("Qual a sua nota: ", escreva1 =>{
+    prompt.question ("Qual a sua Segunda nota: ", escreva2 =>{
+        prompt.question("Qual a sua terceira nota: ", escreva3 =>{
+            const nota1 = Number.parseInt(escreva1)
+            const nota2 = Number.parseInt(escreva2)
+            const nota3 = Number.parseInt(escreva3)
+            const media = (nota1 + nota2 + nota3) / 3
+            if (Number.isNaN(nota1 && nota2 && nota3)){
+                console.log("Um dos capos não foi preenchido corretamente! Tente de novo!")
+                console.log("Preencha com os números (ex: 1, 2, 3, etc.), não por extenso (ex: um, dois, três, etc.)")
+            }else if(nota1 > 10 || nota2 > 10 || nota3 > 10){
+                console.log("Preencha com numeros entra 0 e 10")
+            }else if (media <= 6){
+                console.log (`Sua nota é ${media.toFixed(2)}  e você foi REPROVADO!`);
+             }else if (media <= 8){
+                console.log(`Sua nota foi ${media.toFixed(2)} e voçê passou na MÉDIA!`)
+             }else {
+                console.log(`Sua nota é ${media.toFixed(2)} e você foi APROVADO!`)
+             }
+            prompt.close()
+            
 
-app.get ("/", (req, res) =>{
-    console.log("Entrou Aqui")
-    res.send ("Hello Word")
-})
-
-app.post("/", (req, res) =>{
-    res.send("App Post")
-})
-
-
-app.listen(port, () => {
-console.log (`app online na porta ${port}`)
-
-})
-
+        });
+    });
+});
