209c18d627809459a7c934741dd7db3360175811
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-18 01:40:50 -0300

    Carro e Sair

---
+++ src/LCeSC.js
@@ -0,0 +1,19 @@
+const nchovendo = true
+const guardaC = true
+const roupaL = true
+const cafeM = true
+const banho = true
+const dente = true
+const carroL = true
+const carroG = true
+const temTudo = true
+const garagemA = true
+
+if(nchovendo && guardaC && roupaL && cafeM && banho && dente && carroL && carroG && temTudo && garagemA){
+    console.log("Parabéns, Pode sair de Casa!")
+} else {
+    console.log("Não pode sair de casa")
+}
+
+
+
